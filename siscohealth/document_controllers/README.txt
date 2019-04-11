0ac866ea459655d1b4adf31359e232bbc42624ef
navdeepghai1 <navdeepghai1@gmail.com>
2019-04-11 19:22:02 +0400

    Document Controller: for all the standard documents in ERPNext

---
+++ siscohealth/document_controllers/README.txt
@@ -0,0 +1,2 @@
+# You can define controller  here for all standard erpnext documents
+
---
+++ siscohealth/document_controllers/patient_encounter.py
@@ -0,0 +1,10 @@
+'''
+	Developer Navdeep
+	Email navdeepghai1@gmail.com
+'''
+
+class PatientEncounter(object):
+	
+	def validate_patient_encounter(self):
+		print("Patient controller")
+		print(self.doc.as_dict())
