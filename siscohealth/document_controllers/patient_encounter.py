db5dbe31be25b505043da7bb585b5cbfd0f69bfb
navdeepghai1 <navdeepghai1@gmail.com>
2019-04-16 21:49:11 +0400

    Added controller to manipulate Patient Encounter form

--- siscohealth/document_controllers/patient_encounter.py
+++ siscohealth/document_controllers/patient_encounter.py
@@ -8,3 +8,10 @@ class PatientEncounter(object):
 	def validate_patient_encounter(self):
 		print("Patient controller")
 		print(self.doc.as_dict())
+
+	def update_diagnosis(self):
+		self.patient_intake = frappe.get_doc("Patient Intake", self.doc.patient_intake)
+		self.update_osteoarthritis()
+		self.update_neuropathy()
+		self.update_arthritis()
+		self.update_alergies()	
