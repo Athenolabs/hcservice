70e413da959e98f6af7ac2aff56fd0c07a146991
navdeepghai1 <navdeepghai1@gmail.com>
2019-04-19 13:54:30 +0400

    Added all  My Reports

---
+++ siscohealth/siscohealth/report/lead_provider_lead_stat/lead_provider_lead_stat.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, navdeepghai1@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Lead Provider Lead Stat"] = {
+	"filters": [
+
+	]
+}
